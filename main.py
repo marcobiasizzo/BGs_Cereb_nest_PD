664287fbd23381c133061b0af5a86bedb7559dff
alessandra trapani <alessandramaria.trapani@polimi.it>
2022-08-10 10:02:14 +0200

    data

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ else:
 
 mode_list = ['external_dopa', 'internal_dopa', 'both_dopa']
 experiment_list = ['active', 'EBCC']
-mode = mode_list[0]
+mode = mode_list[1]
 experiment = experiment_list[1]
 
 # defines where the dopamine is depleted
