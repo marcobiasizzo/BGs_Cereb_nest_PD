68aedfec9da31733d09f1d3d35bf45925dfa28ea
Marco Biasizzo <marco.biasizzo@santannapisa.it>
2022-08-10 15:03:50 +0200

    update

--- run_scripts.py
+++ run_scripts.py
@@ -2,7 +2,7 @@ import subprocess
 
 trials = 5
 
-for idx in range(4, 5):
+for idx in range(trials):
     program = [f'./main.py {str(idx + 1)}']
 
     subprocess.call(program, shell=True) # , capture_output=True)
