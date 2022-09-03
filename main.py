79194b01ad945ea3b328df9855274829f3e3e60d
Marco Biasizzo <marco.biasizzo@santannapisa.it>
2022-09-03 19:04:43 +0200

    update, wrong implem

--- main.py
+++ main.py
@@ -55,14 +55,14 @@ else:
     run_on_vm = True
 
 # IO stimulation every trial
-t_start_MF = 1000
-t_start_IO = 1250
-t_end = 1260
+t_start_MF = 500
+t_start_IO = t_start_MF + 250
+t_end = t_start_IO + 10
 stimulation_frequency = 500  # [sp/s]
 
 N_BGs = 20000
 N_Cereb = 96767
-load_from_file = False       # load results from directory or simulate and save
+load_from_file = True       # load results from directory or simulate and save
 dopa_depl_level = -0.0      # between 0. and -0.8
 sol_n = 17
 if dopa_depl_level != 0.:
@@ -82,8 +82,8 @@ if experiment == 'active':
     sim_period = 1.  # ms
     trials = 1
 elif experiment == 'EBCC':
-    settling_time = 0.
-    sim_time = 1760.
+    settling_time = 500.
+    sim_time = t_end + 500  #    1760.
     start_time = 0.  # starting time for histograms data
     sim_period = 10.  # ms
     trials = 5
@@ -106,9 +106,9 @@ nest.set_verbosity("M_ERROR")  # reduce plotted info
 # set saving directory
 # date_time = datetime.now().strftime("%d%m%Y_%H%M%S")
 # savings_dir = f'shared_results/complete_{int(sim_time)}ms_x_{trials}_sol{sol_n}_{mode}_{experiment}'  # f'savings/{date_time}'
-savings_dir = f'savings/complete_{int(sim_time)}ms_x_{trials}_sol{sol_n}_{mode}_{experiment}'  # f'savings/{date_time}'
+savings_dir = f'shared_results/complete_{int(sim_time)}ms_x_{trials}_sol{sol_n}_{mode}_{experiment}'  # f'savings/{date_time}'
 if dopa_depl: savings_dir = savings_dir + f'_dopadepl_{(str(int(-dopa_depl_level*10)))}'
-# if load_from_file: savings_dir += '_trial_1'
+if load_from_file: savings_dir += '_trial_1'
 
 if len(sys.argv) > 1:
     n_trial = int(sys.argv[1])
@@ -208,7 +208,7 @@ if __name__ == "__main__":
     if not load_from_file:
         # create an instance of the populations and inputs
         Cereb_class = C_c(nest, hdf5_path, 'spike_generator', n_spike_generators=500,
-                          mode=mode, experiment=experiment, dopa_depl=dopa_depl_cereb, LTD=-1.0e-3*0.02)
+                          mode=mode, experiment=experiment, dopa_depl=dopa_depl_cereb, LTD=-1.0e-3)
         BGs_class = B_c(nest, N_BGs, 'active', 'BGs_nest/default_params.csv', dopa_depl=dopa_depl_BGs,
                         cortex_type='spike_generator', in_vitro=False,
                         n_spike_generators={'FS': 250, 'M1': 1250, 'M2': 1250, 'ST': 50})
@@ -354,7 +354,7 @@ if __name__ == "__main__":
 
     if experiment == 'EBCC':
         average_fr_per_trial = utils.average_fr_per_trial([rasters], model_dic['pop_ids'], t_end, t_end, settling_time, trials)
-        POP_NAME = 'purkinje'
+        POP_NAME = 'dcn'
         io_idx = [i for i, n in enumerate(recorded_names) if n == POP_NAME][0]
         fig10, ax10 = vsl.plot_fr_learning1([average_fr_per_trial], recorded_names, POP_NAME, labels=[dopa_depl_level])
         # fig10, ax10 = vsl.plot_fr_learning2([instant_fr[io_idx]], t_start, t_end, settling_time, trials, POP_NAME, labels=[dopa_depl_level])
