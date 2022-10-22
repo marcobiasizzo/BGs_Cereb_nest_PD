595f8b3f2e870408f1b1d9bc2545232c54f9dbb5
Marco Biasizzo <marco.biasizzo@santannapisa.it>
2022-10-22 23:18:58 +0200

    update

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ stimulation_frequency = 500  # [sp/s]
 
 N_BGs = 20000
 N_Cereb = 96767
-load_from_file = False       # load results from directory or simulate and save
+load_from_file = True       # load results from directory or simulate and save
 dopa_depl_level = -0.        # between 0. and -0.8
 
 sol_n = 17
@@ -355,7 +355,7 @@ if __name__ == "__main__":
     # fig8.show()
 
     instant_fr = utils.fr_window_step(rasters, model_dic['pop_ids'], settling_time + sim_time*trials, window=10., step=10., start_time=5.)
-    fig9, ax9 = vsl.plot_instant_fr_multiple(instant_fr, clms=1, t_start=start_time, trials=trials, time_range=[500 + 1260*0, 500+1260*4])
+    fig9, ax9 = vsl.plot_instant_fr_multiple(instant_fr, clms=1, t_start=start_time, trials=trials, time_range=[500 + 1260*36, 500+1260*40])
     threshold, CR, reaction_times = utils.calculate_threshold(instant_fr, trials, settling_time, sim_time, t_start_MF,
                                                       t_start_IO, m1=1., q=5., m2=1.2, ax=ax9[2])
     # cum_mean, diff_cu = utils.calculate_cum_mean(instant_fr, trials, settling_time, sim_time, t_start_MF, t_start_IO, m=1.3, ax=ax9[2])
